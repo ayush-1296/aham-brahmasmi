8fd914d3b8e120b330bd092404e8ed41e9aab836
Ayush <56786190+ayush-1296@users.noreply.github.com>
2019-10-21 20:04:15 +0530

    balidaan de diya lol

---
+++ prog/ayush_print.java
@@ -0,0 +1,7 @@
+class SacredGames{
+      public static void main(String[] args)
+      {
+              System.out.println("Aham Brahamasmi\n");
+              System.out.println("\tBalidaan dena hoga...\n");
+      }
+}
